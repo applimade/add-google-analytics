45fb5ab3081020e12a2d00b2204811b5ef9288bd
applimade <info@applimade.com>
2022-08-14 02:27:00 -0700

    Updated class structure for php file

--- add-google-analytics.php
+++ add-google-analytics.php
@@ -18,37 +18,36 @@
         function __construct() {
           add_action( 'wp_head', 'add_google_analytics', 1 );
         }
+
+        function add_google_analytics_settings() {
+          add_settings_section( self::add_google_analytics_settings,
+                                      'Add Google Analytics Settings',
+                                      array( &$this, 'add_google_analytics_settings_section' ),
+                                      'writing' );
+        }
+
+        function add_google_analytics_settings_section() {
+          echo "<p>Google Analytics ID</p>";
+        }
+      
+        function add_google_analytics() {
+
+            ?>
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXX-X"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-XXXXXXXX-X');
+            </script>
+            <?php
+
+            $this->add_google_analytics_settings();
+        }
       }
     }
-
-    function add_google_analytics_settings() {
-      add_settings_section( self::add_google_analytics_settings,
-                                  'Add Google Analytics Settings',
-                                  array( &$this, 'add_google_analytics_settings_section' ),
-                                  'writing' );
-    }
-
-    function add_google_analytics_settings_section() {
-      echo "<p>Google Analytics ID</p>";
-    }
-    
-    function add_google_analytics() {
-
-        ?>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXX-X"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-XXXXXXXX-X');
-        </script>
-        <?php
-
-        $this->add_google_analytics_settings();
-    }
-
    /**
     *
     * end of plugin.
